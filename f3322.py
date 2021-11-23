95dceef76dcf1246b6b27895fb99fada2cc4d4e2
ByXiaoXie <xiaoxiewg@gmail.com>
2021-11-23 20:49:42 +0800

    Fix stack overflow
    Fix stack overflow

--- f3322.py
+++ f3322.py
@@ -1,5 +1,9 @@
 # Update f3322 IP
 # API文档 [https://www.pubyun.com/wiki/帮助:api]
+#
+# Update Log:
+# 2021-11-23 05:26:56 - Fix stack overflow.
+#
 # ByXiaoXie   Www.ByXiaoXie.Com
 
 import time
@@ -8,15 +12,28 @@ import base64
 import urllib
 from urllib import request
 
+domainname = ""
+username = ""
+password = ""
+
+user_info_str = username + ":" + password
+user_info = base64.b64encode(user_info_str.encode())
+
+headers = {
+'Host': 'members.3322.net',
+'Authorization': "Basic " + user_info.decode(),
+'User-Agent': 'myclient/1.0 me@null.net',
+}
+
 def print_log(str):
-    if len(str) <= 2:
+    if not str.strip():
         return
+    str = str.replace("\r","").replace("\n","")
     now = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
     print(now + "：" + str)
 
     with open("f3322.log","a") as file:
-            file.write(now + "：" + str +"\n")
-    
+            file.write(now + "：" + str + "\n")
 
 def GetIp():
     try:
@@ -29,36 +46,29 @@ def GetIp():
         return ipaddress
     except:
         print_log("Get IP Time Out!")
-        time.sleep(5)
-        GetIp()
+        return ""
 
 def UpdateIP():
     try:
-        ipaddress = GetIp()
-        domainname = ""
-        username = ""
-        password = ""
-    
-        user_info_str = username + ":" + password
-        user_info = base64.b64encode(user_info_str.encode())
-
-        headers = {
-        'Host': 'members.3322.net',
-        'Authorization': "Basic " + user_info.decode(),
-        'User-Agent': 'myclient/1.0 me@null.net',
-        }
+        while True:
+            ipaddress = GetIp()
+            if ipaddress != "":
+                break
+            else:
+                time.sleep(5)
+                ipaddress = GetIp()
 
         url = "http://members.3322.net/dyndns/update?hostname=" + domainname + "&myip=" + ipaddress + "&wildcard=OFF&mx=mail.exchanger.ext&backmx=NO&offline=NO"
         req = request.Request(url,headers=headers)
         getpost = request.urlopen(req).read().decode('utf-8')
         print_log(getpost)
         
-        time.sleep(60)
-        UpdateIP()
+        return True
     except:
         print_log("Update Error!")
-        time.sleep(5)
-        UpdateIP()
+        return False
 
 if __name__ == "__main__":
-    UpdateIP()
+    while True:
+        UpdateIP()
+        time.sleep(60)
